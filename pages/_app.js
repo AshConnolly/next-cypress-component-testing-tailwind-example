7b282409c46ce282726f8f597eb63886447e985a
Ash Connolly <ashconnollydigital@gmail.com>
2022-01-27 18:10:50 +0000

    updated _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
-import { css } from '@emotion/react';
-import '../styles/globals.css';
+import { css } from '@emotion/react'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div
       // prettier-ignore
-      css={css`margin: 0; padding: 30px; border: 2px solid red;`} // [1]
+      css={css`margin: 0; padding: 30px;`} // [1]
     >
       <Component {...pageProps} />
     </div>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
